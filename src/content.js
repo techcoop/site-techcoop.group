a60a2282e2c208ecf42e409db9f42847e9ad01c7
Colin Gagnon <gagnon.colin@gmail.com>
2017-09-23 15:31:06 -0400

    feat:Adds json-google-docs and content file, needs config for webpack

---
+++ src/content.js
@@ -0,0 +1,5 @@
+import { Document } from 'json-google-docs'
+
+const content = new Document('https://script.googleusercontent.com/macros/echo?user_content_key=NVfG0BR4bpzn4B_GSoXnMzOCvMEo-b4jLJaIGSjAwkGk2LwT0QdzF4HgUZs2ypjIZ55XfYAGAL4KEdhRjtCCrJFVZzRJ6RLum5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnLd1x65xMrUd4kWJ7wGAxdxOschcJWqy_dmkCwagJSZQOLbmRbHajeRK7jWom-VxoOvlFWeK2glt&lib=MQIOYbA6JCJ3qik6k9TEJiRoYjyRuxRsI')
+
+export default content
